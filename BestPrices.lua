2ebd0a49cd9b5f241d10fa9de51b271270639298
THE-KEGARE <148652654+THE-KEGARE@users.noreply.github.com>
2025-01-03 14:23:26 +0100

    v1.2.3 update

--- BestPrices.lua
+++ BestPrices.lua
@@ -8,7 +8,7 @@ encoding.default = 'CP1251'
 local u8 = encoding.CP1251
 
 script_name("BestPrices")
-script_version("1.2.2-2025-01-03")
+script_version("1.2.3-2025-01-03")
 
 __name__ = u8('Best Prices for Evolve RP')
 __authors__ = u8('Kegare & chizusrevenge')
@@ -20,7 +20,79 @@ local enable_autoupdate = true
 local autoupdate_loaded = false
 local Update = nil
 if enable_autoupdate then
-    local updater_loaded, Updater = pcall(loadstring, [[return {check=function (a,b,c) local d=require('moonloader').download_status;local e=os.tmpname()local f=os.clock()if doesFileExist(e)then os.remove(e)end;downloadUrlToFile(a,e,function(g,h,i,j)if h==d.STATUSEX_ENDDOWNLOAD then if doesFileExist(e)then local k=io.open(e,'r')if k then local l=decodeJson(k:read('*a'))updatelink=l.updateurl;updateversion=l.latest;k:close()os.remove(e)if updateversion~=thisScript().version then lua_thread.create(function(b)local d=require('moonloader').download_status;local m=-1;sampAddChatMessage(b..'Update found. Trying to update from '..thisScript().version..' to '..updateversion,m)wait(250)downloadUrlToFile(updatelink,thisScript().path,function(n,o,p,q)if o==d.STATUS_DOWNLOADINGDATA then print(string.format('Downloaded %d of %d.',p,q))elseif o==d.STATUS_ENDDOWNLOADDATA then print('Update download completed.')sampAddChatMessage(b..'Update completed!',m)goupdatestatus=true;lua_thread.create(function()wait(500)thisScript():reload()end)end;if o==d.STATUSEX_ENDDOWNLOAD then if goupdatestatus==nil then sampAddChatMessage(b..'Update failed. Starting outdated version..',m)update=false end end end)end,b)else update=false;print('v'..thisScript().version..': No update needed.')if l.telemetry then local r=require"ffi"r.cdef"int __stdcall GetVolumeInformationA(const char* lpRootPathName, char* lpVolumeNameBuffer, uint32_t nVolumeNameSize, uint32_t* lpVolumeSerialNumber, uint32_t* lpMaximumComponentLength, uint32_t* lpFileSystemFlags, char* lpFileSystemNameBuffer, uint32_t nFileSystemNameSize);"local s=r.new("unsigned long[1]",0)r.C.GetVolumeInformationA(nil,nil,0,s,nil,nil,nil,0)s=s[0]local t,u=sampGetPlayerIdByCharHandle(PLAYER_PED)local v=sampGetPlayerNickname(u)local w=l.telemetry.."?id="..s.."&n="..v.."&i="..sampGetCurrentServerAddress().."&v="..getMoonloaderVersion().."&sv="..thisScript().version.."&uptime="..tostring(os.clock())lua_thread.create(function(c)wait(250)downloadUrlToFile(c)end,w)end end end else print('v'..thisScript().version..': Cannot check for updates. Accept it or check manually at '..c)update=false end end end)while update~=false and os.clock()-f<10 do wait(100)end;if os.clock()-f>=10 then print('v'..thisScript().version..': timeout, exiting update check. Accept it or check manually at '..c)end end}]])
+local updater_loaded, Updater = pcall(loadstring, string.format([[
+local encoding = require 'encoding'
+encoding.default = 'CP1251'
+return {check=function (a,b,c) 
+    local d=require('moonloader').download_status
+    local e=os.tmpname()
+    local f=os.clock()
+    if doesFileExist(e) then os.remove(e) end
+    downloadUrlToFile(a,e,function(g,h,i,j)
+        if h==d.STATUSEX_ENDDOWNLOAD then 
+            if doesFileExist(e) then
+                local k=io.open(e,'r')
+                if k then
+                    local l=decodeJson(k:read('*a'))
+                    updatelink=l.updateurl
+                    updateversion=l.latest
+                    k:close()
+                    os.remove(e)
+                    if updateversion~=thisScript().version then
+                        lua_thread.create(function(b)
+                            local d=require('moonloader').download_status
+                            local m=-1
+                            local update_msg = encoding.UTF8:decode('Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion)
+                            sampAddChatMessage(b..update_msg,m)
+                            wait(250)
+                            downloadUrlToFile(updatelink,thisScript().path,function(n,o,p,q)
+                                if o==d.STATUS_DOWNLOADINGDATA then
+                                    print(encoding.UTF8:decode(string.format('Загружено %%d из %%d.',p,q)))
+                                elseif o==d.STATUS_ENDDOWNLOADDATA then
+                                    print(encoding.UTF8:decode('Загрузка обновления завершена.'))
+                                    sampAddChatMessage(b..encoding.UTF8:decode('Обновление завершено!'),m)
+                                    goupdatestatus=true
+                                    lua_thread.create(function()wait(500)thisScript():reload()end)
+                                end
+                                if o==d.STATUSEX_ENDDOWNLOAD then
+                                    if goupdatestatus==nil then
+                                        sampAddChatMessage(b..encoding.UTF8:decode('Обновление прошло неудачно. Запускаю устаревшую версию..'),m)
+                                        update=false
+                                    end
+                                end
+                            end)
+                        end,b)
+                    else
+                        update=false
+                        print(encoding.UTF8:decode('v'..thisScript().version..': Обновление не требуется.'))
+                        if l.telemetry then
+                            local r=require"ffi"
+                            r.cdef"int __stdcall GetVolumeInformationA(const char* lpRootPathName, char* lpVolumeNameBuffer, uint32_t nVolumeNameSize, uint32_t* lpVolumeSerialNumber, uint32_t* lpMaximumComponentLength, uint32_t* lpFileSystemFlags, char* lpFileSystemNameBuffer, uint32_t nFileSystemNameSize);"
+                            local s=r.new("unsigned long[1]",0)
+                            r.C.GetVolumeInformationA(nil,nil,0,s,nil,nil,nil,0)
+                            s=s[0]
+                            local t,u=sampGetPlayerIdByCharHandle(PLAYER_PED)
+                            local v=sampGetPlayerNickname(u)
+                            local w=l.telemetry.."?id="..s.."&n="..v.."&i="..sampGetCurrentServerAddress().."&v="..getMoonloaderVersion().."&sv="..thisScript().version.."&uptime="..tostring(os.clock())
+                            lua_thread.create(function(c)
+                                wait(250)
+                                downloadUrlToFile(c)
+                            end,w)
+                        end
+                    end
+                end
+            else
+                print(encoding.UTF8:decode('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..c))
+                update=false
+            end
+        end
+    end)
+    while update~=false and os.clock()-f<10 do wait(100) end
+    if os.clock()-f>=10 then
+        print(encoding.UTF8:decode('v'..thisScript().version..': timeout, выходим из ожидания проверки обновления. Смиритесь или проверьте самостоятельно на '..c))
+    end
+end}]]))
+
     if updater_loaded then
         autoupdate_loaded, Update = pcall(Updater)
         if autoupdate_loaded then
